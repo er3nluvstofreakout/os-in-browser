63cd4e36615ecec37fc843e2884ae2bce9662a91
a <102840087+kingdudely@users.noreply.github.com>
2026-09-06 02:38:14 +0000

    ok

--- forge.config.js
+++ forge.config.js
@@ -1,18 +1,8 @@
+
 module.exports = {
-    packagerConfig: {
-        asar: true
-    },
-
-    rebuildConfig: {},
-
-    makers: [
-        {
-            name: "@electron-forge/maker-zip",
-            platforms: [
-                "win32",
-                "linux",
-                "darwin"
-            ]
-        }
-    ]
+	makers: [
+		{
+			name: "@electron-forge/maker-zip"
+		}
+	]
 }
