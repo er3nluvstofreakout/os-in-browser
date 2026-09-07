c75d2eb8f85a5bec46b560ea04c1478ec68743c3
a <102840087+kingdudely@users.noreply.github.com>
2026-09-07 00:40:57 +0000

    pk

--- forge.config.js
+++ forge.config.js
@@ -1,5 +1,12 @@
 module.exports = {
+	makers: [
+		{
+			name: "@electron-forge/maker-zip",
+			platforms: ["win32", "linux", "darwin"]
+		}
+	],
+
 	rebuildConfig: {
 		ignoreModules: ["native-apis"]
 	}
-}
+};
